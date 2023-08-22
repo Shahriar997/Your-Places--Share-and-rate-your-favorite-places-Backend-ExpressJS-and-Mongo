8db8a358477c85036fc6e4d55d62086c43b910cb
Shahriar Hossain <shahriar.hossain997@gmail.com>
2023-08-23 02:54:23 +0600

    added get coordinates for adress from google api

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -2,6 +2,7 @@ const { v4: uuid } = require('uuid');
 const { validationResult } = require('express-validator')
 
 const HttpError = require("../models/http-error");
+const getCoordsForAddress = require("../util/location");
 
 let DUMMY_PLACES = [
   {
@@ -51,7 +52,10 @@ const createPlace = (req, res, next) => {
     throw new HttpError('Invalid inputs passed. please check your data', 422);
   }
 
-  const { title, description, coordinates, address, creator } = req.body;
+  const { title, description, address, creator } = req.body;
+
+  let coordinates = getCoordsForAddress(address);
+
   const createdPlace = {
     id: uuid(),
     title,
---
+++ util/location.js
@@ -0,0 +1,11 @@
+const API_KEY = "I do not have credit card to get the api";
+
+function getCoordsForAddress(address) {
+  return { 
+    lat: 40.7484474, 
+    lng: -73.9871516,
+    msg: "I am poor. I do not have a credit card to get google API"
+  };
+}
+
+module.exports = getCoordsForAddress;
